2908b0f5d4a29734fbb9995910177e287dc78d1f
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-09-23 18:54:23 +0300

    feat: add some methods to services to test Admins page

--- frontend/src/services/authentication.ts
+++ frontend/src/services/authentication.ts
@@ -1,28 +1,88 @@
+import { ADMIN_STATUS } from '../types/enums';
+
 // Temporary implementation for testing UI
-const TEST_USER: Record<string, string> = {
+const TEST_ADMIN: Record<string, string> = {
   email: 'johnsmith@gmail.com',
   password: '12345',
   firstName: 'John',
   lastName: 'Smith',
+  lastLogin: Date.now().toString(),
+  status: ADMIN_STATUS.ACTIVE,
 };
-const NO_USER_MSG =
+
+const NO_ADMIN_MSG =
   'Long and boring message about the fact that user couldn`t be found or entered password is incorrect. ';
+const NO_RIGHTS_MSG = 'Current admin was blocked or deleted';
+const NO_SUCH_ADMIN_MSG = 'No such admin';
+
+const DELAY = 3000; // ms
 
 // map like {[email: user_obj]}
-const users: Record<string, Record<string, string>> = {
-  [TEST_USER.email]: TEST_USER,
+const admins: Record<string, Record<string, string>> = {
+  [TEST_ADMIN.email]: TEST_ADMIN,
+  [TEST_ADMIN.email]: TEST_ADMIN,
+  [TEST_ADMIN.email]: TEST_ADMIN,
+  [TEST_ADMIN.email]: TEST_ADMIN,
 };
 
 const authenticate = async (data: FormData): Promise<object> => {
-  const delay = 3000; // ms
-
   return new Promise((resolve, reject) => {
-    const user: typeof TEST_USER | undefined =
-      users[data.get('email') as string];
-    if (!user || user.password !== data.get('password'))
-      setTimeout(() => reject(new Error(NO_USER_MSG)), delay);
-    setTimeout(() => resolve(user), delay);
+    const admin: typeof TEST_ADMIN | undefined =
+      admins[data.get('email') as string];
+
+    if (!admin || admin.password !== data.get('password'))
+      setTimeout(() => reject(new Error(NO_ADMIN_MSG)), DELAY);
+
+    setTimeout(() => resolve(admin), DELAY);
   });
 };
 
-export { authenticate };
+const getAdmins = (): Promise<object[]> => {
+  const testVariable = true; // to have the possibility to test UI behavior
+
+  return new Promise((resolve, reject) => {
+    if (!testVariable)
+      setTimeout(() => reject(new Error(NO_RIGHTS_MSG)), DELAY);
+
+    setTimeout(() => resolve(Object.values(admins)), DELAY);
+  });
+};
+
+// identifier is currently email, but later could be INDEX in db
+const blockAdmin = (identifier: string): Promise<object[]> => {
+  const isPresent = identifier in admins;
+
+  return new Promise((resolve, reject) => {
+    if (!isPresent)
+      setTimeout(() => reject(new Error(NO_SUCH_ADMIN_MSG)), DELAY);
+
+    admins[identifier].status = ADMIN_STATUS.BLOCKED;
+    setTimeout(() => resolve(Object.values(admins)), DELAY);
+  });
+};
+
+const unBlockAdmin = (identifier: string): Promise<object[]> => {
+  const isPresent = identifier in admins;
+
+  return new Promise((resolve, reject) => {
+    if (!isPresent)
+      setTimeout(() => reject(new Error(NO_SUCH_ADMIN_MSG)), DELAY);
+
+    admins[identifier].status = ADMIN_STATUS.ACTIVE;
+    setTimeout(() => resolve(Object.values(admins)), DELAY);
+  });
+};
+
+const deleteAdmin = (identifier: string): Promise<object[]> => {
+  const isPresent = identifier in admins;
+
+  return new Promise((resolve, reject) => {
+    if (!isPresent)
+      setTimeout(() => reject(new Error(NO_SUCH_ADMIN_MSG)), DELAY);
+
+    delete admins[identifier];
+    setTimeout(() => resolve(Object.values(admins)), DELAY);
+  });
+};
+
+export { authenticate, getAdmins, blockAdmin, unBlockAdmin, deleteAdmin };
