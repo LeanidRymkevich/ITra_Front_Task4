3a75e68b70bfa3b00f1b4240b8ffe4da385d177d
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-09-23 23:42:12 +0300

    fix: fix Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import { FC } from 'react';
-import { RouterProvider } from 'react-router-dom';
 
-import useRouter from './router/Router';
+import Router from './router/Router';
 
 import { AuthContextElement } from './contexts/AuthContext';
 
 const App: FC = () => {
-  const router = useRouter();
-
   return (
     <AuthContextElement>
-      <RouterProvider router={router} />
+      <Router />
     </AuthContextElement>
   );
 };
--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,8 +1,4 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { privateRoutes, publicRoutes } from './routes';
 
@@ -11,23 +7,27 @@ import Layout from '../components/Layout/Layout';
 import NotFound from '../pages/NotFound/NotFound';
 
 import { ROOT_PATH } from '../constants/constants';
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
+import useAuthState from '../hooks/useAuthState';
+import { FC } from 'react';
 
-const useRouter = () => {
-  const { authToken } = useContext(AuthContext);
+const Router: FC = () => {
+  const { authToken } = useAuthState();
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path={ROOT_PATH} element={<Layout />} errorElement={<Fallback />}>
-        {authToken ? privateRoutes : publicRoutes}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={ROOT_PATH}
+          element={<Layout />}
+          errorElement={<Fallback />}
+        >
+          {authToken ? privateRoutes : publicRoutes}
 
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    )
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-
-  return router;
 };
 
-export default useRouter;
+export default Router;
